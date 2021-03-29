aa1fcdd2159d99d81056b6da6db405516369cfc7
Aditya DS <adithya18062000@gmail.com>
2021-03-29 23:57:37 +0530

    - connecting to mongodb database - passing data from database into templates

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const { getSubjects, getSyllabus } = require('./controllers/subjects');
 
 const app = express();
+
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+}, (err) => {
+    if (err) throw new Error(err);
+    console.log('Connected to DB');
+});
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
@@ -12,12 +25,15 @@ app.get('/dashboard/home', (req, res) => {
     res.render('index');
 });
 
-app.get('/dashboard/subjects', (req, res) => {
-    res.render('subjects');
+app.get('/dashboard/subjects', async (req, res) => {
+    const courses = await getSubjects(10);
+    res.render('subjects', { courses });
 });
 
-app.get('/dashboard/subjects/math', (req, res) => {
-    res.render('manage-subject');
+app.get('/dashboard/subjects/:subject', async (req, res) => {
+    const { subject } = req.params;
+    const syllabus = await getSyllabus(10, subject);
+    res.render('manage-subject', { course: subject, syllabus });
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, () => {
