662a9facf1d61762beb264645266988bb2c04685
Aditya DS <adithya18062000@gmail.com>
2021-03-31 01:52:46 +0530

    router for dashboard

---
+++ routes/dashboard-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { getSubjects, getSyllabus } = require('../controllers/subjects');
+
+router.get('/home', (req, res) => {
+    res.render('index');
+});
+
+router.get('/subjects', async (req, res) => {
+    const courses = await getSubjects(10);
+    res.render('subjects', { courses });
+});
+
+router.get('/subjects/:subject', async (req, res) => {
+    const { subject } = req.params;
+    const syllabus = await getSyllabus(10, subject);
+    res.render('manage-subject', { course: subject, syllabus });
+});
+
+module.exports = router;
