4fbd700ded68301d3dd403d9150299e632e97e15
Aditya DS <adithya18062000@gmail.com>
2021-03-31 01:54:45 +0530

    passport init and code clean up

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const { getSubjects, getSyllabus } = require('./controllers/subjects');
+const passport = require('passport');
+const session = require('cookie-session');
+const authRoutes = require('./routes/auth-routes');
+const dashboardRoutes = require('./routes/dashboard-routes');
+require('./auth/config');
 
 const app = express();
 
@@ -15,25 +19,21 @@ mongoose.connect(process.env.DB_URI, {
 });
 
 app.use(express.static('public'));
+app.use(session({
+    name: 'Pomodoro',
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+    },
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/', authRoutes);
+app.use('/dashboard', dashboardRoutes);
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.redirect('/dashboard/home');
-});
-
-app.get('/dashboard/home', (req, res) => {
-    res.render('index');
-});
-
-app.get('/dashboard/subjects', async (req, res) => {
-    const courses = await getSubjects(10);
-    res.render('subjects', { courses });
-});
-
-app.get('/dashboard/subjects/:subject', async (req, res) => {
-    const { subject } = req.params;
-    const syllabus = await getSyllabus(10, subject);
-    res.render('manage-subject', { course: subject, syllabus });
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, () => {
