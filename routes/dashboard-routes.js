a0558da89c2dbbd85d1b788387e5d9c04736ef6b
Aditya DS <adithya18062000@gmail.com>
2021-03-31 02:49:12 +0530

    added avatar instead of grey circle

--- routes/dashboard-routes.js
+++ routes/dashboard-routes.js
@@ -5,18 +5,18 @@ const { hasAuthenticated } = require('../middlewares/authentication');
 router.use(hasAuthenticated);
 
 router.get('/home', (req, res) => {
-    res.render('index');
+    res.render('index', { avatar: req.user.avatar });
 });
 
 router.get('/subjects', async (req, res) => {
     const courses = await getSubjects(10);
-    res.render('subjects', { courses });
+    res.render('subjects', { courses, avatar: req.user.avatar });
 });
 
 router.get('/subjects/:subject', async (req, res) => {
     const { subject } = req.params;
     const syllabus = await getSyllabus(10, subject);
-    res.render('manage-subject', { course: subject, syllabus });
+    res.render('manage-subject', { course: subject, syllabus, avatar: req.user.avatar });
 });
 
 module.exports = router;
