e8c53387d6a35838def90ec86fb080b1056cb370
Aditya DS <adithya18062000@gmail.com>
2021-04-01 15:31:13 +0530

    change bg color on clicking tick

--- public/src/js/tick.js
+++ public/src/js/tick.js
@@ -1,7 +1,12 @@
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
 import updateTopicStatus from './update-topic-status';
 
 const ticks = document.querySelectorAll('.feather-check');
 
 ticks.forEach((tick) => {
-    tick.addEventListener('click', () => updateTopicStatus.apply(tick, ['/revisedTopic']));
+    tick.addEventListener('click', function () {
+        this.parentElement.parentElement.parentElement.style.backgroundColor = '#82FFBC';
+        updateTopicStatus.apply(tick, ['/revisedTopic']);
+    });
 });
