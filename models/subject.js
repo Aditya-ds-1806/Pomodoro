ee324bfc334970be70f27adb59af10799ab4e69a
Aditya DS <adithya18062000@gmail.com>
2021-04-02 00:34:48 +0530

    new field in subjectSchema

--- models/subject.js
+++ models/subject.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const subjectSchema = new mongoose.Schema({
     chapter: String,
     topics: [String],
+    weightage: Number,
 }, { _id: false });
 
 module.exports = { schema: subjectSchema };
