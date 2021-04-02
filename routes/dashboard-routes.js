592f47bbed74e3f430f8c08cd06d1eb719fbcf2a
Aditya DS <adithya18062000@gmail.com>
2021-04-02 12:37:06 +0530

    added last revised date

--- routes/dashboard-routes.js
+++ routes/dashboard-routes.js
@@ -1,6 +1,7 @@
+const moment = require('moment');
 const router = require('express').Router();
 const { getSubjects, getSyllabus } = require('../controllers/subjects');
-const { getNewTopics, getRevisingTopics } = require('../controllers/topics');
+const { getNewTopics, getRevisingTopics, getRevisedTopics } = require('../controllers/topics');
 const { hasAuthenticated } = require('../middlewares/authentication');
 
 router.use(hasAuthenticated);
@@ -17,8 +18,22 @@ router.get('/subjects', async (req, res) => {
 });
 
 router.get('/subjects/:subject', async (req, res) => {
+    const { user } = req;
     const { subject } = req.params;
     const syllabus = await getSyllabus(10, subject);
+    const revisedTopics = await getRevisedTopics(user._id);
+    syllabus.map((entry) => {
+        /* eslint-disable no-param-reassign */
+        entry.lastRevised = moment(revisedTopics
+            .filter(({ chapter }) => entry.chapter === chapter)
+            .sort(({ date: date1 }, { date: date2 }) => date1 - date2)
+            ?.pop()?.date || null).fromNow();
+        if (entry.lastRevised === 'Invalid date') entry.lastRevised = 'Never';
+        console.log(entry.lastRevised);
+        return entry;
+        /* eslint-enable no-param-reassign */
+    });
+    console.log(syllabus);
     res.render('manage-subject', { course: subject, syllabus, avatar: req.user.avatar });
 });
 
