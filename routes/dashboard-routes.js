1701a57fde41b3bf089f0b58f77f28485204857c
Aditya DS <adithya18062000@gmail.com>
2021-04-05 15:19:27 +0530

    - using onboarding middleware - routes for onboarding

--- routes/dashboard-routes.js
+++ routes/dashboard-routes.js
@@ -1,26 +1,47 @@
 const moment = require('moment');
-const router = require('express').Router();
+const express = require('express');
+
+const router = express.Router();
 const { getSubjects, getSyllabus } = require('../controllers/subjects');
 const { getNewTopics, getRevisingTopics, getRevisedTopics } = require('../controllers/topics');
 const { hasAuthenticated } = require('../middlewares/authentication');
+const { updateGradeAndCycle } = require('../controllers/users');
+const { hasOnboarded, hasNotOnboarded } = require('../middlewares/onboarding');
 
 router.use(hasAuthenticated);
 
-router.get('/home', async (req, res) => {
+router.get('/get-started', hasNotOnboarded, (req, res) => {
+    console.log('get-started');
+    res.render('onboarding', { avatar: req.user.avatar, home: true });
+});
+
+router.post('/get-started', hasNotOnboarded, express.urlencoded({ extended: true }), async (req, res) => {
+    await updateGradeAndCycle(req.user._id, req.body);
+    res.redirect('/dashboard/home');
+});
+
+router.get('/home', hasOnboarded, async (req, res) => {
     const newTopics = await getNewTopics(req.user._id);
     const revisingTopics = await getRevisingTopics(req.user._id);
-    res.render('index', { avatar: req.user.avatar, newTopics, revisingTopics });
+    res.render('index', {
+        avatar: req.user.avatar,
+        grade: req.user.grade,
+        newTopics,
+        revisingTopics,
+    });
 });
 
-router.get('/subjects', async (req, res) => {
-    const courses = await getSubjects(10);
-    res.render('subjects', { courses, avatar: req.user.avatar });
+router.get('/subjects', hasOnboarded, async (req, res) => {
+    const { grade } = req.user;
+    const courses = await getSubjects(grade);
+    res.render('subjects', { courses, avatar: req.user.avatar, grade: req.user.grade });
 });
 
-router.get('/subjects/:subject', async (req, res) => {
+router.get('/subjects/:subject', hasOnboarded, async (req, res) => {
     const { user } = req;
+    const { grade } = user;
     const { subject } = req.params;
-    const syllabus = await getSyllabus(10, subject);
+    const syllabus = await getSyllabus(grade, subject);
     const revisedTopics = await getRevisedTopics(user._id);
     syllabus.map((entry) => {
         /* eslint-disable no-param-reassign */
@@ -32,8 +53,12 @@ router.get('/subjects/:subject', async (req, res) => {
         return entry;
         /* eslint-enable no-param-reassign */
     });
-    console.log(syllabus);
-    res.render('manage-subject', { course: subject, syllabus, avatar: req.user.avatar });
+    res.render('manage-subject', {
+        course: subject,
+        syllabus,
+        avatar: req.user.avatar,
+        grade: req.user.grade,
+    });
 });
 
 module.exports = router;
