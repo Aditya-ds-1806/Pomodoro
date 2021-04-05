a05415a4d289b0a8571968d46c8be73b4d16db3b
Aditya DS <adithya18062000@gmail.com>
2021-04-05 15:20:33 +0530

    replaced 10 with dynamic value

--- public/src/js/mark-distribution.js
+++ public/src/js/mark-distribution.js
@@ -1,6 +1,8 @@
 import plot from './plot';
 
 const subject = window.location.pathname.split('/').pop();
-fetch(`/markDistribution/10/${subject}`)
+const { grade } = document.querySelector('nav img').dataset;
+console.log(grade);
+fetch(`/markDistribution/${grade}/${subject}`)
     .then((res) => res.json())
     .then((data) => plot('distribution', data));
