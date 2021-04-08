b59807909081f47e722c9607584262ab739f6a6d
Aditya DS <adithya18062000@gmail.com>
2021-04-08 20:25:01 +0530

    disable card on plus

--- public/src/js/gradient.js
+++ public/src/js/gradient.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
 fetch('/revisedTopic')
     .then((res) => res.json())
     .then(({ revisionCycle, topics }) => {
@@ -5,5 +7,31 @@ fetch('/revisedTopic')
             const ms = revisionCycle * 24 * 60 * 60 * 1000;
             const percentage = ((Date.now() - Date.parse(date)) / ms) * 200;
             document.querySelector(`[data-topic='${topic}']`).parentElement.parentElement.style.background = `linear-gradient(270deg, #82FFBC 0%, #E09D8C ${200 - percentage}%, #FF7C7C 100%)`;
+        });
+    });
+
+fetch('/revisingTopic')
+    .then((res) => res.json())
+    .then(({ topics }) => {
+        topics.forEach(({ topic }) => {
+            document.querySelector(`[data-topic='${topic}']`).parentElement.parentElement.style.background = '#a09f9fde';
+            document.querySelectorAll(`[data-topic='${topic}'] svg`).forEach((icon) => {
+                const clonedIcon = icon.cloneNode(true);
+                clonedIcon.style.cursor = 'not-allowed';
+                icon.replaceWith(clonedIcon);
+            });
+        });
+    });
+
+fetch('/newTopic')
+    .then((res) => res.json())
+    .then(({ topics }) => {
+        topics.forEach(({ topic }) => {
+            document.querySelector(`[data-topic='${topic}']`).parentElement.parentElement.style.background = '#a09f9fde';
+            document.querySelectorAll(`[data-topic='${topic}'] svg`).forEach((icon) => {
+                const clonedIcon = icon.cloneNode(true);
+                clonedIcon.style.cursor = 'not-allowed';
+                icon.replaceWith(clonedIcon);
+            });
         });
     });
--- public/src/js/plus.js
+++ public/src/js/plus.js
@@ -1,7 +1,16 @@
+/* eslint-disable no-param-reassign */
 import updateTopicStatus from './update-topic-status';
 
 const plusses = document.querySelectorAll('.feather-plus');
 
 plusses.forEach((plus) => {
-    plus.addEventListener('click', () => updateTopicStatus.apply(plus, ['/addTopic']));
+    plus.addEventListener('click', () => {
+        updateTopicStatus.apply(plus, ['/addTopic']);
+        [...plus.parentElement.children].forEach((icon) => {
+            const clonedIcon = icon.cloneNode(true);
+            icon.parentElement.parentElement.parentElement.style.background = '#a09f9fde';
+            clonedIcon.style.cursor = 'not-allowed';
+            icon.replaceWith(clonedIcon);
+        });
+    });
 });
